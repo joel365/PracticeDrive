8bc63134863b215df674ffaa4619b829ae9e0b4d
OzzieShazam <35015649+OzzieShazam@users.noreply.github.com>
2019-02-04 15:24:40 -0500

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,32 +7,16 @@
 
 package frc.robot;
 
-
-
 /**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
+ * Add your docs here.
  */
 public enum RobotMap {
-    //motor mapping
-    LEFT_MOTOR_1(14),
-    LEFT_MOTOR_2(15),
-    RIGHT_MOTOR_1(13),
-    RIGHT_MOTOR_2(12),
-    //joy stick mappings
-    LEFT_JOYSTICK(0),
-    RIGHT_JOYSTICK(1);
-public final int value;
-
-RobotMap(int value){
-    this.value = value;
-
+    LEFTMOTOR_1(1), LEFTMOTOR_2(4), RIGHTMOTOR_1(2), RIGHTMOTOR_2(3),
+    JOYSTICK_LX(0), JOYSTICK_LY(1), JOYSTICK_LT(2),
+    JOYSTICK_RX(4), JOYSTICK_RY(5), JOYSTICK_RT(3),
+    ;
+    public int value;
+    RobotMap(int value){
+        this.value = value;
+    }
 }
-
-
-
-
- 
-} 
