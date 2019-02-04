560715d5b38b3ee07f2563ddbf0df24212708983
OzzieShazam <35015649+OzzieShazam@users.noreply.github.com>
2019-02-04 15:25:20 -0500

    Update DriveBase.java

--- src/main/java/frc/robot/subsystems/DriveBase.java
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -1,61 +1,71 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.subsystems;
-
-
-import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.Robot;
-import frc.robot.RobotMap;
-import frc.robot.commands.TankDrive;
-
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+package frc.robot.Subsystems;
 
 /**
- * An example subsystem. You can replace me with your own Subsystem.
+ * Add your docs here.
  */
-public class DriveBase extends Subsystem /*implements PIDOutput*/ {
 
-  private TalonSRX Leftmotor_1;
-  private TalonSRX Leftmotor_2;
-  private TalonSRX Rightmotor_1;
-  private TalonSRX Rightmotor_2;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.Commands.*;
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.*;
 
-  //public final PIDController turnController;
+public class DriveBase extends Subsystem {
 
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-  public DriveBase() {
-    Leftmotor_1 = new TalonSRX(RobotMap.LEFT_MOTOR_1.value);
-    Leftmotor_2 = new TalonSRX(RobotMap.LEFT_MOTOR_2.value);
-    Rightmotor_1 = new TalonSRX(RobotMap.RIGHT_MOTOR_1.value);
-    Rightmotor_2 = new TalonSRX(RobotMap.RIGHT_MOTOR_2.value);
+    private TalonSRX _leftMaster = new TalonSRX(RobotMap.LEFTMOTOR_1.value);
+	private TalonSRX _rightMaster = new TalonSRX(RobotMap.RIGHTMOTOR_1.value);
+	private TalonSRX _leftSlave = new TalonSRX(RobotMap.LEFTMOTOR_2.value);
+	private TalonSRX _rightSlave = new TalonSRX(RobotMap.RIGHTMOTOR_2.value);
 
-    Robot.intTalon(Leftmotor_1);
-    Robot.intTalon(Leftmotor_2);
-    Robot.intTalon(Rightmotor_1);
-    Robot.intTalon(Rightmotor_2);
+    public DriveBase() {
+        /* Ensure motor output is neutral during init */
+		_leftMaster.set(ControlMode.PercentOutput, 0);
+		_rightMaster.set(ControlMode.PercentOutput, 0);
+		_leftSlave.set(ControlMode.PercentOutput, 0);
+		_rightSlave.set(ControlMode.PercentOutput, 0);
 
-    Leftmotor_2.follow(Leftmotor_1);
-    Rightmotor_2.follow(Rightmotor_1);
-  }
+		/* Factory Default all hardware to prevent unexpected behaviour */
+		_leftMaster.configFactoryDefault();
+		_rightMaster.configFactoryDefault();
+		_leftSlave.configFactoryDefault();
+		_rightSlave.configFactoryDefault();
+		
+		/* Set Neutral mode */
+		_leftMaster.setNeutralMode(NeutralMode.Brake);
+		_rightMaster.setNeutralMode(NeutralMode.Brake);
+		_leftSlave.setNeutralMode(NeutralMode.Brake);
+		_rightSlave.setNeutralMode(NeutralMode.Brake);
+		
+		/* Configure output direction */
+		_leftMaster.setInverted(false);
+		_rightMaster.setInverted(true);
+		
+		_leftSlave.follow(_leftMaster);
+		_rightSlave.follow(_rightMaster);
 
-  public void set(ControlMode mode, Double leftvalue1, Double rightvalue1) {
-    Leftmotor_1.set(mode, leftvalue1);
-    Rightmotor_1.set(mode, rightvalue1);
-  }
+    }
 
-  @Override
-  public void initDefaultCommand() {
-    setDefaultCommand(new TankDrive());
-  }
+    public void arcadeSet(double forward, double turn){
+        /* Arcade Drive using PercentOutput along with Arbitrary Feed Forward supplied by turn */
+		_leftMaster.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, +turn);
+		_rightMaster.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, -turn);
+    }
 
- /* public void pidWrite(double output) {
-    set(ControlMode.PercentOutput, -output, output);
-  }*/
+    public void tankSet(double left, double right){
+        /* Arcade Drive using PercentOutput along with Arbitrary Feed Forward supplied by turn */
+		_leftMaster.set(ControlMode.PercentOutput, left, DemandType.ArbitraryFeedForward, 0);
+		_rightMaster.set(ControlMode.PercentOutput, right, DemandType.ArbitraryFeedForward, 0);
+    }
+
+    @Override
+    public void initDefaultCommand() {
+        setDefaultCommand(new ArcadeDrive());
+    }
 }
